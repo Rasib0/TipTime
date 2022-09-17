7f20ff5dcb83ab966a75617854d28cebeb3fc1f5
RasibNadeem175 <rasibnadeem101@gmail.com>
2022-09-17 09:36:47 +0500

    added comments

--- app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -5,8 +5,7 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.containsString
@@ -21,11 +20,13 @@ class CalculatorTests {
     val activity = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun calculate_20_percent_tip() {
-        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("50.00")).perform(
+    fun calculate_15_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("100.00")).perform(
             ViewActions.closeSoftKeyboard()
         )
+
+        onView(withId(R.id.option_fifteen_percent)).perform(click())
         onView(withId(R.id.calculate_button)).perform(click())
-        onView(withId(R.id.tip_result)).check(matches(withText(containsString("$10.00"))))
+        onView(withId(R.id.tip_result)).check(matches(withText(containsString("$15.00"))))
     }
 }
--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -14,15 +14,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //setting up event listeners
         binding.calculateButton.setOnClickListener { calculateTip() }
         binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
             handleKeyEvent(view, keyCode)
         }
     }
-
+    //Calculate the tip based on the options chosen
     private fun calculateTip() {
         val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
@@ -39,21 +41,21 @@ class MainActivity : AppCompatActivity() {
                 0.15
             }
         }
-
         var tip = tipPercentage * cost
 
         if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
         displayTip(tip)
-
     }
 
+    //format the tip amount with currency format and send that string to tipResult
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 
+    //hide the keyboard when the value is entered
     private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
         if (keyCode == KeyEvent.KEYCODE_ENTER) {
             // Hide the keyboard
